e3a62c8f0c50df6bcf4b1c97bb6bdd962a6709a4
KingsleyUsoroeno <kingsleyenou@gmail.com>
2020-05-06 12:49:34 +0100

    added app screenshot

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:covid19/main.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_testart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
